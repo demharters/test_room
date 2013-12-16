c105d0d198e68bab1b16abb2a7e2a078fef0bd2d
demharters <samuel.demharter@seh.ox.ac.uk>
2013-12-16 16:56:16 +0000

    Added function to extract lowest energy models

--- get_energies.py
+++ get_energies.py
@@ -4,9 +4,10 @@ import operator
 class edit_files(object):
 
    
-    def __init__(self,my_file_input):
+    def __init__(self,my_file_input,my_pdb_file):
 
         self.my_file = my_file_input
+        self.my_pdb_file = my_pdb_file
 
 
     def clean_file(self):
@@ -54,13 +55,13 @@ class edit_files(object):
             self.my_temperature_minima.append(self.my_temperature_minima[i-1] + period)
             self.no_minima += 1
 
-    def write_models(self,my_pdb_file):
+    def write_models(self):
     # Takes full trajectory as input and returns trajectory filtered for
     # values in self.my_temperature_minima.
         
         my_pdb_output = open("minima.pdb","w")
 
-        with open(my_pdb_file) as f:
+        with open(self.my_pdb_file) as f:
             
             parsing = False
             i = 1
@@ -95,6 +96,47 @@ class edit_files(object):
 
         my_pdb_output.close()
 
+    def write_low_energy_models(self,no_low_energy_models):
+
+        low_energy_pdb = open("low_energy.pdb","w")
+
+        self.the_chosen_ones = self.my_sorted_keys[:no_low_energy_models]
+        
+        for model in self.the_chosen_ones:
+
+
+            with open(self.my_pdb_file) as f:
+
+                parsing = False
+                i = 1
+
+                for line in f:
+    
+                    if "MODEL" in line and int(line.split()[1]) == model:
+    
+                        parsing = True
+   
+                        print(str(line.split()[0]),str(i),file = low_energy_pdb)
+    
+                        i += 1
+    
+                    elif "MODEL" in line and int(line.split()[1]) != model:
+   
+                       parsing = False
+
+                    else:
+                        pass
+    
+
+
+                    if "MODEL" not in line and parsing:
+
+                        print(line.rstrip("\n"),file = low_energy_pdb)
+
+                    else:
+                        pass
+        
+
     def write_energy(self):
         
         my_energy_output = open("energy.dat","w")
@@ -118,10 +160,11 @@ class edit_files(object):
             print(str(self.my_sorted_keys[i]).ljust(self.col_width),
                     str(self.my_sorted_values[i]).ljust(self.col_width))
 
-energy_file = edit_files("sampled.pot_energy")
+energy_file = edit_files("sampled.pot_energy","sampled.pos.pdb")
 my_energies = energy_file.clean_file()
 energy_file.sort_values()
 #energy_file.print_values(10)
 energy_file.filter_values(4)
-energy_file.write_models("sampled.pos.pdb")
-energy_file.write_energy()
+energy_file.write_models()
+#energy_file.write_energy()
+#energy_file.write_low_energy_models(50)
